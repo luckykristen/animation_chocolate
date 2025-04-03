921437d6e8b1eae639127b352e852a2b0cdadede
Kristina Kalíková <129938050+luckykristen@users.noreply.github.com>
2025-04-03 15:54:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+gsap.from(".header", { duration: 2, opacity: 0})
+
+gsap.from(".link", {opacity: 0, duration: 3, stagger: 2})
+
+gsap.from(".video", {y: 80, ease: "back", duration: 5, opacity: 0})
+
+gsap.from(".btn", {y: 10, opacity: 0, duration: 2, delay: 5})
